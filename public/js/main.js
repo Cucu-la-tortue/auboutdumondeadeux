5f81a178ba9e1e9a8919d080a0864182802f8b71
Cucu-la-tortue <cyprien.cursoux@gmail.com>
2022-04-28 15:01:56 +0200

    First commit

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+/* ---------------------------------------------------------- */
+/* ------------------------- Footer ------------------------- */
+/* ---------------------------------------------------------- */
+// Get the button
+var scrollButton = document.querySelector(".footer__move-up");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+
+/* ---------------------------------------------------------- */
+/* --------------------------- AOS -------------------------- */
+/* ---------------------------------------------------------- */
+AOS.init();
